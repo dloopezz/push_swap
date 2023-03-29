9cafa4e3a1ec8b0abd541c44158da26d5697bf4d
dlopez-s <dlopez-s@student.42.com>
2023-03-29 18:12:35 +0200

    .

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,8 +6,69 @@
 /*   By: dlopez-s <dlopez-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 16:57:30 by dlopez-s          #+#    #+#             */
-/*   Updated: 2023/03/27 17:14:37 by dlopez-s         ###   ########.fr       */
+/*   Updated: 2023/03/29 18:04:43 by dlopez-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// er pu sua
+#include "push_swap.h"
+
+t_node	*add_node(t_node *lst, long num)
+{
+	t_node	*new;
+	t_node	*aux;
+
+	new = (t_node *)malloc(sizeof(t_node));
+	new->data = num;
+	new->next = NULL;
+	if (!lst)
+		lst = new;
+	else
+	{
+		aux = lst;
+		while(aux->next)
+		{
+			aux = aux->next;
+			aux->next = new;
+		}
+	}
+	return (lst);
+}
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	long		nb;
+	char	**arr;
+	t_node	*lst;
+
+	if (argc >= 1)
+	{
+		i = 1;
+		while (argv[i])
+		{
+			arr = ft_split(argv[i], SPACE);
+			if (!arr[1])
+			{
+				nb = ft_atoi(argv[i]);
+				lst = add_node(lst, nb);
+				printf("Node content: %li\n", lst->data);
+				lst = lst->next;
+			}
+			else
+			{
+				j = 0;
+				while (arr[j])
+				{
+					nb = ft_atoi(arr[j]);
+					lst = add_node(lst, nb);
+					printf("Node content: %li\n", lst->data);
+					lst = lst->next;
+					j++;
+				}
+			}
+			i++;
+		}
+	}
+	return (0);
+}
--- src/push_swap.h
+++ src/push_swap.h
@@ -6,17 +6,26 @@
 /*   By: dlopez-s <dlopez-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 17:02:35 by dlopez-s          #+#    #+#             */
-/*   Updated: 2023/03/27 17:11:48 by dlopez-s         ###   ########.fr       */
+/*   Updated: 2023/03/29 18:04:36 by dlopez-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
 #include <stdio.h>
 #include <stdlib.h>
+
+# include "../libft/libft.h"
+
+# define SPACE 32 
 
 //#define NODE struct s_node
 
 typedef struct s_node
 {
-	void			*data;
+	long				data;
 	struct s_node	*next;
 }				t_node;
+
+#endif
