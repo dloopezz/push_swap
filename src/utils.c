5e36f1eb7ec7af3dff94284cbdc2a5b1c57c66cc
dlopez-s <dlopez-s@student.42.com>
2023-03-31 13:29:50 +0200

    control de errores terminado

--- src/utils.c
+++ src/utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lopezz <lopezz@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dlopez-s <dlopez-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 12:49:26 by dlopez-s          #+#    #+#             */
-/*   Updated: 2023/03/30 21:51:34 by lopezz           ###   ########.fr       */
+/*   Updated: 2023/03/31 12:28:58 by dlopez-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	error_found(char *str)
 	exit (EXIT_FAILURE);
 }
 
-//problemas con " 3" o " "
+//problemas con " 3", "3 " o " "
 long long	ft_atoi_ps(const char *str)
 {
 	int			i;
@@ -36,11 +36,11 @@ long long	ft_atoi_ps(const char *str)
 			sign *= -1;
 		i++;
 	}
-	if (((str[i] < '0' || str[i] > '9') && str[i]) || ft_strlen(str) == 0)
+	if (((str[i] < '0' || str[i] > '9')) || ft_strlen(str) == 0)
 		error_found("Error\n");
 	while (str[i] >= '0' && str[i] <= '9')
 		nb = (str[i++] - '0') + (nb * 10);
-	if ((str[i] < '0' || str[i] > '9') && str[i])
+	if ((str[i] < '0' || str[i] > '9') && str[i] && str[i] != SPACE)
 		error_found("Error\n");
 	nb *= sign;
 	if (nb > INT_MAX || nb < INT_MIN)
